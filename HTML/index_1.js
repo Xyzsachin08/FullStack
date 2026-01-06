308e155396de99e8e2be3aca4b87263e273286c7
Sachin Borude <sachinborude0610@gmail.com>
2026-01-06 14:21:39 +0530

    added

---
+++ HTML/index_1.js
@@ -0,0 +1,12 @@
+document.write("hello word")
+{
+    var a = "sachin";
+    var d = 23;
+    var c = [1, 5, 6, 9];
+
+}
+console.log(a, d);
+console.log(typeof c);
+function sachin {
+    document.getElementById("b1").innerHTML = "Hello moto";
+}
